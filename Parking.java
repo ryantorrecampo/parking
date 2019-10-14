952453e40f10b1f84616bfc1661600e019cc4487
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-10-14 14:05:43 -0500

    initial boiler

---
+++ Parking.java
@@ -0,0 +1,15 @@
+
+public class Parking {
+
+    class ParkingLot {
+        int capacity;
+    }
+
+    class Cars {
+        float time;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
